7867117cd0519b5376ac2c02c0592b49ad8093bf
Pedro Palmeira <34588988+Peticali@users.noreply.github.com>
2021-07-11 19:03:24 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,17 @@
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name="BlurWindow",
+    version="1.0.1",
+    description="Blur PySide windows.",
+    url="https://github.com/Peticali/PythonBlurBehind",
+    author="Peticali",
+    author_email="pedropalmeira68@gmail.com",
+    license="MIT",
+    packages=['BlurWindow']
+    
+    
+
+)
