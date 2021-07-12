74b5fe78d75c68658deeb973a842097445111f2a
Pedro Palmeira <34588988+Peticali@users.noreply.github.com>
2021-07-12 16:54:36 -0300

    Update tkinter BLUR.py

--- tkinter BLUR.py
+++ tkinter BLUR.py
@@ -1,7 +1,7 @@
 from tkinter import *
 import ctypes
 
-from BlurWindow.blurWindow import blur
+from BlurWindow.blurWindow import blur,Win7Blur
 
 root = Tk()
 root.config(bg='green')
@@ -12,10 +12,18 @@ root.update()
 
 global HWND
 HWND = ctypes.windll.user32.GetForegroundWindow()
-blur(HWND,Acrylic=True)
+blur(HWND)
+
 
 def color(hex):
     blur(HWND,hexColor=hex)
+
+def color2(hex):
+    blur(HWND,hexColor=hex,Acrylic=True)
+
+def Win7():
+    Win7Blur(HWND)
+
 
 e = Entry(width=9)
 e.insert(0,'#12121240')
@@ -23,9 +31,10 @@ e.insert(0,'#12121240')
 e.pack()
 b = Button(text='Apply',command=lambda:[color(e.get())])
 b.pack()
+b = Button(text='ApplyAcrylic',command=lambda:[color2(e.get())])
+b.pack()
+b = Button(text='Win7Blur',command=Win7)
+b.pack()
 
 
 root.mainloop()
-
-
-
