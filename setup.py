2cd7ee6c439841db2107777427c5a3c42f04848c
Pedro Palmeira <34588988+Peticali@users.noreply.github.com>
2021-08-05 17:32:42 -0300

    Upgraded to 1.2.1
    Fixes PyQt5 in Windows 10

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.rst").read_text()
 
 setup(
     name="BlurWindow",
-    version="1.2.0",
+    version="1.2.1",
     description="Blur PySide, Tkinter, etc windows.",
     url="https://github.com/Peticali/PythonBlurBehind",
     author="Peticali",
